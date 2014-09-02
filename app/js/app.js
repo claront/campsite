a9ac170be82a9bd225956e9246a843aa6bb2d74a
Claron Twitchell <claront@gmail.com>
2014-09-02 14:26:22 -0600

    change myApp to campsiteApp in various files

--- app/js/app.js
+++ app/js/app.js
@@ -2,12 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('campsiteApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'campsiteApp.filters',
+  'campsiteApp.services',
+  'campsiteApp.directives',
+  'campsiteApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
