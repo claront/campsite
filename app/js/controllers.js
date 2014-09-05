f9d91b4ef2d705762a2cf75728340f04e2ebf544
Claron Twitchell <claront@gmail.com>
2014-09-04 22:47:58 -0600

    Put wording (content) that sounds like campsite.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,13 +4,13 @@
 
 angular.module('campsiteApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-  	$scope.phones = [
-	    {'name': 'Nexus S',
-	     'snippet': 'Fast just got faster with Nexus S.'},
-	    {'name': 'Motorola XOOM™ with Wi-Fi',
-	     'snippet': 'The Next, Next Generation tablet.'},
-	    {'name': 'MOTOROLA XOOM™',
-	     'snippet': 'The Next, Next Generation tablet.'}
+  	$scope.campsites = [
+	    {'name': 'Lambs Canyon',
+	     'snippet': 'Tall green grass, beautiful, but freeway noise'},
+	    {'name': 'Jordanell Resevoir',
+	     'snippet': 'Nice view of the Resevoir and Deer Valley, no overnight camping allowed.'},
+	    {'name': 'Church pavilion in Woodland, Utah',
+	     'snippet': 'Cold night. No one kicked me out.'}
   ];
 
   }])
@@ -18,6 +18,7 @@ angular.module('campsiteApp.controllers', [])
 
   }]);
 
+/*
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
@@ -30,3 +31,4 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
      'snippet': 'The Next, Next Generation tablet.'}
   ];
 });
+*/
