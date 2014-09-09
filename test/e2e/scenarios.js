16aca209440624df8f4ff7621c4a9212930f5056
Claron Twitchell <claront@gmail.com>
2014-09-09 02:36:50 -0600

    update the e2e test to be CampSite App

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,7 +2,7 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
-describe('my app', function() {
+describe('CampSite App', function() {
 
   browser.get('index.html');
 
