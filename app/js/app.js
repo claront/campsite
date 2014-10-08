76538c7fc8078f161973a0649f059c2608657791
Claron Twitchell <claront@gmail.com>
2014-10-08 14:24:05 -0600

    a little formating of the code while I am experimenting

--- app/js/app.js
+++ app/js/app.js
@@ -10,8 +10,12 @@ angular.module('campsiteApp', [
   'campsiteApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'CampSiteCtrl'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {
+    templateUrl: 'partials/partial1.html', 
+    controller: 'CampSiteCtrl'});
+  $routeProvider.when('/view2', {
+    templateUrl: 'partials/partial2.html', 
+    controller: 'MyCtrl2'});
   // commented out the .otherwise to enable the test/e2e/scenarios.js 
   // test to work for it('should render campsite specific links',...
   // $routeProvider.otherwise({redirectTo: '/view1'});
