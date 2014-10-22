9b31488d06ba3f98b84fe220d6c0c88d9e3a33d4
Claron Twitchell <claront@gmail.com>
2014-10-21 23:00:00 -0600

    added unit test for services

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -5,10 +5,17 @@
 describe('service', function() {
   beforeEach(module('campsiteApp.services'));
 
-
   describe('version', function() {
     it('should return current version', inject(function(version) {
       expect(version).toEqual('0.1');
     }));
   });
+
+  // load modules
+  beforeEach(module('campsiteguideServices'));
+
+  // Test service availability
+  it('check the existence of Campsite factory', inject(function(Campsite) {
+      expect(Campsite).toBeDefined();
+    }));
 });
