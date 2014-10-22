207f9db394c11bbc9d57220bf565b9acc6071b5d
Claron Twitchell <claront@gmail.com>
2014-10-21 22:17:27 -0600

    factoring out the lower-level  service, replacing it with a new service called Campsite

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,21 +3,17 @@
 /* Controllers */
 
 angular.module('campsiteApp.controllers', [])
-  .controller('CampSiteCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('campsites/campsites.json').success(function(data) {
-	  $scope.campsites = data;
-	});
+  .controller('CampSiteCtrl', ['$scope', 'Campsite', function($scope, Campsite) {
+    $scope.campsites = Campsite.query();
 
-  $scope.orderProp = 'state';
+  	$scope.orderProp = 'state';
   }])
 
-  .controller('CampsiteDetailCtrl', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
-    $http.get('campsites/' + $routeParams.campsiteId + '.json').success(function(data) {
-      $scope.campsite = data;
-      $scope.mainImageUrl = data.images[0];
+  .controller('CampsiteDetailCtrl', ['$scope', '$routeParams', 'Campsite',
+  function($scope, $routeParams, Campsite) {
+    $scope.campsite = Campsite.get({campsiteId: $routeParams.campsiteId}, function(campsite) {
+      $scope.mainImageUrl = campsite.images[0];
     });
-
 
     $scope.setImage = function(imageUrl) {
       $scope.mainImageUrl = imageUrl;
